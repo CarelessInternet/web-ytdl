3f63b60054eb7125cb7b82b326bcf3e9b24d7004
CarelessInternet <myinternetisg00d@gmail.com>
2021-09-27 21:03:11 +0200

    setted up testing scripts

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,81 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+beforeEach(() => {
+  jest.setTimeout(20 * 1000);
+})
+
+test('have download button', () => {
   render(<App />);
-  expect(screen.getByText('hi there')).toBeInTheDocument();
+  expect(screen.getByRole('button')).toBeInTheDocument();
+});
+
+test('have input field with rickroll link', () => {
+  render(<App />);
+  expect(screen.getByPlaceholderText('https://www.youtube.com/watch?v=dQw4w9WgXcQ')).toBeInTheDocument();
+});
+
+test('download button disables after click', () => {
+  render(<App />);
+
+  const button = screen.getByRole('button');
+  button.click();
+
+  expect(button).toBeDisabled();
+});
+
+test('get invalid youtube video url error', async () => {
+  render(<App />);
+
+  const input = screen.getByPlaceholderText('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  userEvent.type(input, 'sample text');
+  
+  const button = screen.getByRole('button');
+  button.click();
+
+  await waitFor(() => {
+    expect(screen.getByText('An invalid Youtube video URL was provided')).toBeInTheDocument();
+  });
+});
+
+test('download me at the zoo youtube video (0:18 long)', async () => {
+  render(<App />);
+
+  const input = screen.getByPlaceholderText('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  userEvent.type(input, 'https://www.youtube.com/watch?v=jNQXAC9IVRw');
+  
+  const button = screen.getByRole('button');
+  button.click();
+
+  await waitFor(() => {
+    expect(screen.getByRole('source')).toBeInTheDocument();
+  }, {
+    timeout: 20 * 1000
+  });
+});
+
+test('download ksi & lil wayne - lose youtube video (3:33 long) and 10 min countdown (10:16 long)', async () => {
+  render(<App />);
+
+  const input = screen.getByPlaceholderText('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  userEvent.type(input, 'https://www.youtube.com/watch?v=r5inVONkOKQ');
+  
+  const button = screen.getByRole('button');
+  button.click();
+
+  await waitFor(() => {
+    expect(screen.getByRole('source')).toBeInTheDocument();
+  }, {
+    timeout: 20 * 1000
+  });
+
+  userEvent.type(input, 'https://www.youtube.com/watch?v=4ASKMcdCc3g');
+  button.click();
+
+  await waitFor(() => {
+    expect(screen.getByRole('source')).toBeInTheDocument();
+  }, {
+    timeout: 20 * 1000
+  });
 });
